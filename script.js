532eaf8f07734b53186560ebd623852618c72174
Juston Hart <contact@justonh.art>
2024-01-29 15:18:35 -0600

    Add resize function
    Make grid resizable, up to width of 100

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const container = document.getElementById('container');
+const darken = document.getElementById('darken');
 
-for(let i = 0; i < 16; i++){
-	const row = document.createElement('div');
-	row.classList.add('row');
-	for(let j = 0; j < 16; j++){
-		const cell = document.createElement('div');
-		cell.classList.add('cell');
-		row.appendChild(cell);
+function render(size = 16){
+	container.innerHTML = '';
+
+	for(let i = 0; i < size; i++){
+		const row = document.createElement('div');
+		row.classList.add('row');
+		for(let j = 0; j < size; j++){
+			const cell = document.createElement('div');
+			cell.classList.add('cell');
+			row.appendChild(cell);
+		}
+		container.appendChild(row);
 	}
-	container.appendChild(row);
 }
+
+const resizeBtn = document.getElementById('resizeBtn');
+resizeBtn.addEventListener('click', () => {
+	const newSize = prompt('Enter a new grid size (max: 100)');
+	if(newSize <= 100){
+		render(newSize);
+	}
+});
 
 addEventListener('mouseover', (event) => {
 	if(event.target.classList.contains('cell')){
-		event.target.classList.toggle('black');
+		if(darken.checked){
+		} else {
+			event.target.classList.toggle('black');
+		}
 	}
 });
+
+render();
